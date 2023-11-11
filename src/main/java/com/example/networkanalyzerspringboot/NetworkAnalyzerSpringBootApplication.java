338f690df685316990ddba3e558f271f986eace5
Illia Ponomarov <chippaxyucac@gmail.com>
2023-11-11 15:50:03 +0100

    init

---
+++ src/main/java/com/example/networkanalyzerspringboot/NetworkAnalyzerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.networkanalyzerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkAnalyzerSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkAnalyzerSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/networkanalyzerspringboot/NetworkAnalyzerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.networkanalyzerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkAnalyzerSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
