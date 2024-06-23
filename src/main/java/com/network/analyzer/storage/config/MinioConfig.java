b67ecb1575ffa9a4ce5b64e39a1d714b87e88f4f
Illia Ponomarov <xponomarov@stuba.sk>
2024-06-23 14:09:43 +0200

    configs was updated

---
+++ src/main/java/com/network/analyzer/storage/config/MinioConfig.java
@@ -0,0 +1,39 @@
+package com.network.analyzer.storage.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.url}")
+    private String url;
+
+    @Value("${minio.access-key}")
+    private String accessKey;
+
+    @Value("${minio.secret-key}")
+    private String secretKey;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(url)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
